dcd282431c83ca23f598633598ee2b0f409ceb71
adouenibvs <adouenibvs@gmail.com>
2026-04-02 14:59:39 +0000

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "REMPLACE_API_KEY",
-    authDomain: "REMPLACE_AUTH_DOMAIN",
-    projectId: "REMPLACE_PROJECT_ID",
-    storageBucket: "REMPLACE_STORAGE_BUCKET",
-    messagingSenderId: "REMPLACE_MESSAGING_SENDER_ID",
-    appId: "REMPLACE_APP_ID",
+    apiKey: "AIzaSyATd1OhxpBY8WZLK0m_9TmXx4BbKgF3v1c",
+    authDomain: "tontine-plus-b461a.firebaseapp.com",
+    projectId: "tontine-plus-b461a",
+    storageBucket: "tontine-plus-b461a.firebasestorage.app",
+    messagingSenderId: "590237425395",
+    appId: "1:590237425395:web:e45a1ee93546396a7de186",
+      measurementId: "G-2WEN3T6CEF"
 };
